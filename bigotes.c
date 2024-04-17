ac78c30b56b96336de05e56bfa0b6e5112a3ed36
Rodrigo Arias Mallo <rodarima@gmail.com>
2024-04-17 17:23:14 +0200

    Use the relative SEM as cutoff rule

--- bigotes.c
+++ bigotes.c
@@ -179,63 +179,63 @@ cmp_double(const void *pa, const void *pb)
 		return 0;
 }
 
-static void
-resample(double *values, long n, double *out)
-{
-	for (long i = 0; i < n; i++) {
-		/* FIXME: Not really uniform */
-		out[i] = values[rand() % n];
-		//printf("out[%ld] = %e\n", i, out[i]);
-	}
-}
-
-static double
-mad_bootstrap(double *values, long n)
-{
-	long m = 200;
-
-	double *r = safe_calloc(n, sizeof(double));
-	double *absdev = safe_calloc(n, sizeof(double));
-	double *mad = safe_calloc(m, sizeof(double));
-
-	for (long sample = 0; sample < m; sample++) {
-		resample(values, n, r);
-
-		qsort(r, n, sizeof(double), cmp_double);
-		double median = r[n / 2];
-
-		for (long i = 0; i < n; i++) {
-			absdev[i] = fabs(r[i] - median);
-		}
-
-		qsort(absdev, n, sizeof(double), cmp_double);
-		mad[sample] = absdev[n / 2];
-		//mad[sample] = median;
-		//printf("mad[%ld] = %e\n", sample, mad[sample]);
-	}
-
-	double sum = 0.0;
-	for (long i = 0; i < m; i++)
-		sum += mad[i];
-
-	double mean = sum / (double) m;
-	double sumsqr = 0.0;
-	for (long i = 0; i < m; i++) {
-		double dev = mad[i] - mean;
-		sumsqr += dev * dev;
-	}
-
-	double var = sumsqr / m;
-	double stdev = sqrt(var);
-	double sem = stdev / sqrt(m);
-	double rsem = 100.0 * sem * 1.96 / mean;
-
-	free(mad);
-	free(absdev);
-	free(r);
-
-	return rsem;
-}
+//static void
+//resample(double *values, long n, double *out)
+//{
+//	for (long i = 0; i < n; i++) {
+//		/* FIXME: Not really uniform */
+//		out[i] = values[rand() % n];
+//		//printf("out[%ld] = %e\n", i, out[i]);
+//	}
+//}
+//
+//static double
+//mad_bootstrap(double *values, long n)
+//{
+//	long m = 200;
+//
+//	double *r = safe_calloc(n, sizeof(double));
+//	double *absdev = safe_calloc(n, sizeof(double));
+//	double *mad = safe_calloc(m, sizeof(double));
+//
+//	for (long sample = 0; sample < m; sample++) {
+//		resample(values, n, r);
+//
+//		qsort(r, n, sizeof(double), cmp_double);
+//		double median = r[n / 2];
+//
+//		for (long i = 0; i < n; i++) {
+//			absdev[i] = fabs(r[i] - median);
+//		}
+//
+//		qsort(absdev, n, sizeof(double), cmp_double);
+//		mad[sample] = absdev[n / 2];
+//		//mad[sample] = median;
+//		//printf("mad[%ld] = %e\n", sample, mad[sample]);
+//	}
+//
+//	double sum = 0.0;
+//	for (long i = 0; i < m; i++)
+//		sum += mad[i];
+//
+//	double mean = sum / (double) m;
+//	double sumsqr = 0.0;
+//	for (long i = 0; i < m; i++) {
+//		double dev = mad[i] - mean;
+//		sumsqr += dev * dev;
+//	}
+//
+//	double var = sumsqr / m;
+//	double stdev = sqrt(var);
+//	double sem = stdev / sqrt(m);
+//	double rsem = 100.0 * sem * 1.96 / mean;
+//
+//	free(mad);
+//	free(absdev);
+//	free(r);
+//
+//	return rsem;
+//}
 
 static void
 stats(struct sampling *s)
@@ -257,7 +257,7 @@ stats(struct sampling *s)
 	double q1 = NAN;
 	double q3 = NAN;
 	double iqr = NAN;
-	double pol = NAN;
+	//double pol = NAN;
 	double smin = s->samples[s->n - 1];
 	double smax = s->samples[s->n - 1];
 
@@ -304,8 +304,8 @@ stats(struct sampling *s)
 		}
 		qsort(absdev, s->n, sizeof(double), cmp_double);
 		mad = absdev[s->n / 2] * 1.4826;
-		rsemad = mad_bootstrap(s->samples, s->n);
-		pol = (double) outliers * 100.0 / n;
+		//rsemad = mad_bootstrap(s->samples, s->n);
+		//pol = (double) outliers * 100.0 / n;
 
 		var = sumsqr / n;
 		stdev = sqrt(var);
@@ -341,8 +341,8 @@ stats(struct sampling *s)
 				"  %5s\n",
 				"RUN", "WALL",
 				"MIN", "Q1", "MEDIAN", "Q3", "MAX",
-				"MAD", "%MAD", "%EMAD",
-				"%OUTLIERS");
+				"MAD", "%MAD", "%SEM",
+				"OUTLIERS");
 	}
 //RUN   WALL       LAST     MEDIAN        AVG         SD   %RSD   %RSEM
 // 89  125.5  5.085e-03  5.075e-03  5.303e-03  3.500e-03  66.00   7.611
@@ -352,11 +352,11 @@ stats(struct sampling *s)
 			"\r%4ld  %5.1f"
 			"  %8.2e  %8.2e  %8.2e  %8.2e  %8.2e"
 			"  %8.2e  %5.2f  %5.2f"
-			"  %5.1f ",
+			"  %8ld ",
 			s->n, s->wall,			/* progress */
 			smin, q1, median, q3, smax,	/* centrality */
-			mad, rmad, emad,		/* rel. dispersion */
-			pol				/* outliers */
+			mad, rmad, s->rsem,		/* rel. dispersion */
+			outliers			/* outliers */
 		);
 	fflush(stdout);
 
@@ -378,14 +378,14 @@ should_continue(struct sampling *s)
 	if (s->n < s->nmin)
 		return 1;
 
-	//if (s->rsem > s->min_rsem)
-	//	return 1;
-
-	if (isnan(s->emad) || s->emad > s->min_emad)
+	if (isnan(s->rsem) || s->rsem > s->min_rsem)
 		return 1;
 
-	//if (s->wall < s->min_time)
-	//	return 1;
+//	if (isnan(s->emad) || s->emad > s->min_emad)
+//		return 1;
+
+	if (s->wall < s->min_time)
+		return 1;
 
 	return 0;
 }
@@ -512,11 +512,11 @@ static int
 sample(const char *cmd)
 {
 	struct sampling s = { 0 };
-	s.nmax = 4000;
+	s.nmax = 100000;
 	s.nmin = 100;
-	s.min_rsem = 0.5;
+	s.min_rsem = 2.0;
 	s.min_emad = 1.0;
-	s.min_time = 60.0; /* At least one minute */
+	s.min_time = 10.0;
 	s.samples = safe_calloc(s.nmax, sizeof(double));
 	s.n = 0;
 	s.name = cmd;
